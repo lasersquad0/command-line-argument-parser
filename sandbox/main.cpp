bd41c34a56601c1444caf8c2165d1c2c85763b7b
lasersquad0 <lasersquad@gmail.com>
2026-09-18 23:32:43 +0300

    Added example of defining mutually exclusive options

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -10,9 +10,11 @@ int main(int argc, char *argv[])
     COptionsList options;
     CCommandLine cmd;
 
+    // define list of all options supported by application
     options.AddOption("i", "input", "input test", 2, true);
     options.AddOption("o", "output", "output test", 2, false);
     
+    //another way to define option
     COption opt;
     opt.ShortName("a").LongName("algorithm").Descr("algorithm test").NumArgs(0).RequiredArgs(0).Required(false);
     options.AddOption(opt);
@@ -25,18 +27,17 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    //work with options with 1 and more arguments
-    std::string input = cmd.GetOptionValue("i", 1, "second arg"); // looking for option by short name and index 1
-    printf("-i <arg 2>: %s\n\n", input.c_str());
+    // work with options with 1 and more arguments. Argument indexes start from 0.
+    std::string input = cmd.GetOptionValue("i", 1, "second arg"); // looking in cmd for option by short name and having index 1
+    printf("-i <arg 2>: %s\n\n", input.c_str()); // second argument has index 1
 
-    input = cmd.GetOptionValue("input", 0, "first arg"); // looking for option by long name and index 0
+    input = cmd.GetOptionValue("input", 0, "first arg"); // looking for option by long name and having index 0
     printf("--input <arg 1>: %s\n", input.c_str());
 
 
     //option names are CASE SENSITIVE, the default value "second arg" will be returned because there is no 'I' option in the 'options' list
     input = cmd.GetOptionValue("I", 1, "second arg default"); 
     printf("-I <arg 2>: %s\n\n", input.c_str());
-
 
     std::string output = cmd.GetOptionValue("o", 0, "first arg");
     printf("-o <arg 1>: %s\n", output.c_str());
@@ -55,6 +56,11 @@ int main(int argc, char *argv[])
 
     // all options arguments can be returned all together as vector. it might be more convenient in some cases. 
     vector_string_t arguments = cmd.GetOptionValues("i");
+
+    // defining mutually exclusive options
+    options["i"]->Excludes(options["o"]); // options i and o are mutually exclusive
+    options.MutuallyExclusive("output", "a"); // it's ok to mix short and long options here
+
 
     return 0;
 }
